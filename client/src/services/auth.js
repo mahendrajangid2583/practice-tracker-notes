0debd744afef33993726b8aeeb6c92c920b76b79
Mahendra Jangid <mahendrajangid2583@gmail.com>
2026-03-09 13:46:43 +0530

    mirnor changes

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Dynamic API Base URL (Dev vs Prod)
 const API_BASE = import.meta.env.PROD
-    ? import.meta.env.VITE_API_URL+"/api"
+    ? import.meta.env.VITE_API_URL
     : "http://localhost:5001/api";
 
 const authApi = axios.create({
